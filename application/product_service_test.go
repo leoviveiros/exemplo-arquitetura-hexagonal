0aea016e257100d85fe8583a41ebbc16fd3db38a
Leonardo Pinto <leoviveiros@gmail.com>
2022-04-18 11:21:04 -0300

    implementacao teste product service

---
+++ application/product_service_test.go
@@ -0,0 +1,29 @@
+package application_test
+
+import (
+	"testing"
+
+	"go-hexagonal/application"
+	mock_application "go-hexagonal/application/mocks"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductService_Get(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	product := mock_application.NewMockProductInterface(ctrl)
+	peristence := mock_application.NewMockProductPersistenceInterface(ctrl)
+	peristence.EXPECT().Get(gomock.Any()).Return(product, nil).AnyTimes()
+
+	service := application.ProductService{
+		Persistence: peristence,
+	}
+
+	result, err := service.Get("1")
+
+	require.Nil(t, err)
+	require.Equal(t, product, result)
+}
